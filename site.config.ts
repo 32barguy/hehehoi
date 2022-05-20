72fd04f307db657d547b1a3d1207970a5b2ad1c9
32barguy <92068634+32barguy@users.noreply.github.com>
2022-05-20 17:24:53 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: 'e1cee9371d0944d2bb70efc35af0e27b',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
@@ -14,7 +14,7 @@ export default siteConfig({
   author: 'Remo Antony Francis',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: 'BIO',
 
   // social usernames (optional)
   twitter: 'rEmOaNtOnY6',
