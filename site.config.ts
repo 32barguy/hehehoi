130bc6aac28a575475ac7c9fb450de2b48bfdfd6
32barguy <92068634+32barguy@users.noreply.github.com>
2022-05-20 16:56:31 +0530

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,7 +9,7 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
+  name: 'TEST BOI',
   domain: 'Link-in- B I O ',
   author: 'Remo Antony Francis',
 
